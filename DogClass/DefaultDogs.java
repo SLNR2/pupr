1a563f847b01ed234ddadc6b2f8691b96adee1cf
Sloan Lipman <sloan.lipman@gmail.com>
2018-09-30 18:21:31 -0400

    some minor tweaks, nothing functional yet

--- DogClass/DefaultDogs.java
+++ DogClass/DefaultDogs.java
@@ -9,7 +9,7 @@ public class DefaultDogs {
    
  //  System.out.print(sourceImage);
    
-   Dog megan = new Dog("Megan", "Sloan's dog", new Picture("C:\\Users\\sloan\\Google Drive\\Grad School\\KSU\\2018 - Fall\\SWE 6623\\SWE 6623 Group Project\\Java code\\Megan.jpg")); //need to do something about local directory issues
+   Dog megan = new Dog("Megan", "sloan.lipman@gmail.com", "Look how cute she is!!! 5 stars please!", new Picture("C:\\Users\\sloan\\Google Drive\\Grad School\\KSU\\2018 - Fall\\SWE 6623\\SWE 6623 Group Project\\Java code\\Megan.jpg")); //need to do something about local directory issues
    
    
    }
--- DogClass/Dog.java
+++ DogClass/Dog.java
@@ -6,11 +6,14 @@ public class Dog {
    private Picture pic = new Picture("Megan.jpg");
    private double score = 0;
    private int numberOfVotes = 0;
+   private String owner = "";
   
-   public Dog(String newName, String newBio, Picture newPic) {
+   public Dog(String newName, String newBio, String newOwner, Picture newPic) {
       this.name = newName;
       this.bio = newBio;
+      this.owner = newOwner; //When we have a class for a user to upload a dog, we will need to have a method to pull the identity of the owner from the user's email address
       this.pic = newPic;
+      
       
    }
  
