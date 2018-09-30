d1d1644b8200d9f4f241c49bf78546b827f61a5d
Sloan Lipman <sloan.lipman@gmail.com>
2018-09-30 13:43:00 -0400

    Changed scoring system in dog.java

--- DogClass/Dog.java
+++ DogClass/Dog.java
@@ -4,7 +4,8 @@ public class Dog {
    private String name = "Dog";
    private String bio = "This is a profile for a dog!";
    private Picture pic = new Picture("Megan.jpg");
-   private int score = 0;
+   private double score = 0;
+   private int numberOfVotes = 0;
   
    public Dog(String newName, String newBio, Picture newPic) {
       this.name = newName;
@@ -17,4 +18,12 @@ public class Dog {
       this.name = newName;
       this.bio = newBio;      
    }  
+   
+   public void vote (int numberOfStars) {
+      if (numberOfVotes == 0)
+         this.score = numberOfStars;
+         
+      else
+         this.score = (score * numberOfVotes + numberOfStars) / (numberOfVotes + 1);
+   }
 }
