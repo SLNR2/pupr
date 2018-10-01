a2e5c86ff3637d82fefb948a969b20e62c62f60c
Sloan Lipman <sloan.lipman@gmail.com>
2018-09-30 21:10:40 -0400

    touched up more scoring things

--- DogClass/Dog.java
+++ DogClass/Dog.java
@@ -8,19 +8,25 @@ public class Dog {
    private double averageScore = 0;
    private int numberOfVotes = 0;
    private String owner = "";
+   
+   private int dogID = 0;
   
    public Dog(String newName, String newBio, String newOwner, Picture newPic) {
       this.name = newName;
       this.bio = newBio;
       this.owner = newOwner; //When we have a class for a user to upload a dog, we will need to have a method to pull the identity of the owner from the user's email address
       this.pic = newPic;
+      this.dogID = dogID; //assign a unique identifier to the object
+      dogID++; //increment the unique identifier
       
       
    }
  
     public Dog(String newName, String newBio) {
       this.name = newName;
-      this.bio = newBio;      
+      this.bio = newBio;
+      this.dogID = dogID;
+      dogID++;      
    }  
    
    public void vote (int numberOfStars) {
