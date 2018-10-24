4066bc44a5490e5ea5d11ce1cfc46f74d1e56df0
Sloan Lipman <sloan.lipman@gmail.com>
2018-10-23 20:07:26 -0400

    Forgot to add some changes in the last commit

--- JavaFiles/DBTest.java
+++ JavaFiles/DBTest.java
@@ -6,9 +6,15 @@ import java.io.IOException;
 
 public class DBTest {
    public static void main (String args[]) throws SQLNonTransientConnectionException, IOException, SQLException {
-     Connection con = Database.connect();
-      Statement stmt = con.createStatement();
-      Database.disconnect();
+      Connection con = Database.connect(); //connects to DB and also stores the connection as a variable in this class so we can do some manipulations
+      Statement stmt = con.createStatement(); //
+      ResultSet rs = stmt.executeQuery("desc user");
+  
+//Keep printing stuff out until you get to the end   
+      while(rs.next()) 
+         System.out.println(rs.getString(1));
+      
+      Database.disconnect(); //disconnect from the DB
    }
 
 }
