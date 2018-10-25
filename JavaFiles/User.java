e4c1df504f082824970342a39f9a9f8437b66a1a
Sloan Lipman <sloan.lipman@gmail.com>
2018-10-24 22:08:27 -0400

    Worked on User class and made a TestUser class to test my methods

--- JavaFiles/User.java
+++ JavaFiles/User.java
@@ -1,22 +1,83 @@
 /*
 This program will define the User class.
 
-To create new users, a different class with a main method will invoke the setter method here and then upload to the MySQL databsae using JDBC
+
+Make sure to add throws SQLNonTransientConnectionException, IOException, SQLException when appropriate
 
 */
 
-public class User {
-   private int userId = 0;
-   private int dogId = 0;
-   private String userName = "";
+//import java.util.Date;
+
+import java.sql.*;
+import java.io.IOException;
+
+
+public class User { 
+   private int userId;
+   
+   private String firstName = "";
+   private String lastName = "";
+   private String dateOfBirth = ""; //change to Date class eventually?
+   private String username = "";
    private String email = "";
    private String password = "";
    
-   public User(int newUserId, String newUserName, String newEmail, String newPassword) {
-      this.userId = newUserId;
-      this.userName = newUserName;
-      this.email = newEmail;
-      this.password = newPassword;
+   public User (String fName, String lName, String dob, String uname, String emailAddr, String pass) throws SQLNonTransientConnectionException, IOException, SQLException {
+   //Connect to database
+   
+      this.firstName = fName;
+      this.lastName = lName;
+      this.dateOfBirth = dob;
+      this.username = uname;
+      this.email = emailAddr;
+      this.password = pass;
+      Connection con = Database.connect();
+      Statement stmt = con.createStatement();      
+      
+      String sql = String.format("INSERT INTO user(firstName, lastName, dateOfBirth, username, email, password) VALUES('%s', '%s', '%s', '%s', '%s', '%s'", this.firstName, this.lastName, this.dateOfBirth, this.username, this.email, this.password);
+      
+
+      //Store values in database
+      stmt.executeUpdate(sql);
+      
+//    }   
+}     
+   
+   public void login(){
    
    }
+   
+   public void logout(){
+   
+   }
+   
+   public void viewAccount(){
+   
+   
+   }
+   
+   public void updateAccount(){
+   
+   
+   }
+   
+   public void deleteAccount() {
+   
+   
+   }
+   /*
+   public String getFirstName() {
+   //return first name
+   }
+   
+   public String getLastName() {
+   
+   //return last name
+   }
+   
+   public String getUsername() {
+   //return username
+   
+   }*/
+   
 }
