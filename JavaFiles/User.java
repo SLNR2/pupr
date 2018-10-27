f7aa150c9822b0c0ecd8b8eb2746aa4c89e9e80e
Sloan Lipman <sloan.lipman@gmail.com>
2018-10-27 12:54:39 -0400

    Made the layout for the Dog class, started working on the VotingQueue

--- JavaFiles/User.java
+++ JavaFiles/User.java
@@ -2,7 +2,7 @@
 This program will define the User class.
 
 
-Make sure to add throws SQLNonTransientConnectionException, IOException, SQLException when appropriate
+Make sure to add throws SQLNonTransientConnectionException, IOException, SQLException when appropriate for each method
 
 */
 
@@ -22,62 +22,63 @@ public class User {
    private String email = "";
    private String password = "";
    
+//Constructor method that will use passed values to create a new account and log it into the database
    public User (String fName, String lName, String dob, String uname, String emailAddr, String pass) throws SQLNonTransientConnectionException, IOException, SQLException {
-   //Connect to database
    
+   //Use the values passed into the call   
       this.firstName = fName;
       this.lastName = lName;
       this.dateOfBirth = dob;
       this.username = uname;
       this.email = emailAddr;
       this.password = pass;
+   
+   //Connect to database
+
       Connection con = Database.connect();
       Statement stmt = con.createStatement();      
       
+   //SQL query
       String sql = String.format("INSERT INTO user(firstName, lastName, dateOfBirth, username, email, password) VALUES('%s', '%s', '%s', '%s', '%s', '%s')", this.firstName, this.lastName, this.dateOfBirth, this.username, this.email, this.password);
       
 
-      //Store values in database
-      stmt.executeUpdate(sql);
-      
-//    }   
+   //Store values in database
+      stmt.executeUpdate(sql);   
 }     
    
    public void login(){
-   
+      /*Use a select statement for the entered username and password. If the combination returns a row in the table, access the next screen.
+               Otherwise, display an error screen.*/   
    }
    
    public void logout(){
-   
+      //Click a buttton, move to logout confirmation screen, make any necessary updates to the user's statistics. Maybe clear out the queue or something as a way of doing our own garbage collection.
    }
    
    public void viewAccount(){
-   
+   //Make calls to the different getter methods and display the correct information where it needs to go
    
    }
    
    public void updateAccount(){
-   
+   //Need to make overloaded methods for this to update different aspects
    
    }
    
    public void deleteAccount() {
-   
+      /*Access database, select the user's ID, delete that row from the table.
+            Need to test how this works first. With some foreign key dependencies, we might need to delete their dog first. Also will need to delete the dogs from the vote list?
+      */
    
    }
-   /*
-   public String getFirstName() {
-   //return first name
-   }
    
-   public String getLastName() {
+//Need to go back and add connection method here to return the appropriate names.
+//Do these methods need to pass a userId to return the correct user?
+   public String getFirstName() throws SQLNonTransientConnectionException, IOException, SQLException  {return this.firstName;}
    
-   //return last name
-   }
+   public String getLastName() throws SQLNonTransientConnectionException, IOException, SQLException  {return this.lastName}
    
-   public String getUsername() {
-   //return username
+   public String getUsername() throws SQLNonTransientConnectionException, IOException, SQLException  {return this.username;
    
-   }*/
-   
+   }   
 }
---
+++ JavaFiles/VotingQueue.java
@@ -0,0 +1,57 @@
+public class VotingQueue {
+   private Node first, last;
+   private int size;
+  
+   //constructor method to create a Queue object with first, last, and size. 
+   public VotingQueue()
+   {
+      first = null;  // pointer for the front of the queue
+      last = null;  // pointer for the end of the queue
+      size = 0;     // queue size
+   }
+  
+//Add a dog to the queue
+//Need to add an algorithm to connect to DB, check for dogs that the user has not voted on
+   public void enqueue(int data) 
+   {
+      if (last == null) 
+         first = last = new Node(data); //empty queue
+      else 
+      {
+         last.next = new Node(data); //link new node as last node
+         last = last.next; //make last pointer points to new last node
+      }
+      size++; //increase list size by one
+   }
+
+//Serve the Dog at the front of the Queue            
+   public void dequeue(){
+         Node temp = first; //temp points to the first node
+         first = first.next; //make first point to the second node
+         temp.next = null; //remove the link from the queue
+         size--; //decrement size of the list
+         if (first == null) last = null; //if there was only one node, list is now empty 
+    }
+   
+   
+   //Return the first dog at the front of the queue for the user to see prior to voting. Will need to display the fields in the appropriate spots of the GUI.
+   public int front() {
+      if (first != null)
+         return this.first.data;
+      else return -1; //if no elements in the queue
+      }   
+
+  
+   //class to create nodes of the queue as objects
+   private class Node
+   {
+      private int data;  //data field
+      private Node next; //link field
+       
+      public Node(int item) //constructor method
+      {
+         data = item;
+         next = null;
+      }
+   }
+}
