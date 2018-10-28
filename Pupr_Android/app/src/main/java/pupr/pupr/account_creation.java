be24899914b3637c9a708d47eb82b6865a193e52
Sloan Lipman <sloan.lipman@gmail.com>
2018-10-28 14:13:12 -0400

    App compiles and launches, but there are some connectivity issues still.

--- Pupr_Android/app/src/main/java/pupr/pupr/account_creation.java
+++ Pupr_Android/app/src/main/java/pupr/pupr/account_creation.java
@@ -1,37 +1,45 @@
 package pupr.pupr;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class account_creation extends AppCompatActivity {
 
 //Load text from the boxes on the account creation page
-    EditText firstName = (EditText)findViewById(R.id.firstName);
-    EditText lastName = (EditText)findViewById(R.id.lastName);
-    EditText emailAddr = (EditText)findViewById(R.id.email);
-    EditText firstPass = (EditText)findViewById(R.id.password);
-    EditText confirmPass = (EditText)findViewById(R.id.confirmPassword);
-
-    String fname = firstName.getText().toString();
-    String lname = lastName.getText().toString();
-    String email = emailAddr.getText().toString();
-    String pass = firstPass.getText().toString();
-    String confirmPassword = confirmPass.getText().toString();
-
+    EditText firstName;
+    EditText lastName;
+    EditText emailAddr;
+    EditText firstPass;
+    EditText confirmPass;
+    TextView registered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_account_creation);
+
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        emailAddr = findViewById(R.id.email);
+        firstPass = findViewById(R.id.password);
+        confirmPass = findViewById(R.id.confirmPassword);
+
+        String fname = firstName.getText().toString();
+        String lname = lastName.getText().toString();
+        String email = emailAddr.getText().toString();
+        String pass = firstPass.getText().toString();
+        String confirmPassword = confirmPass.getText().toString();
+
+        registered = findViewById(R.id.success);
+
 
         Button register =  findViewById(R.id.register); //find the button
 
@@ -41,7 +49,10 @@ public class account_creation extends AppCompatActivity {
                 try {
                     if (pass.equals(confirmPassword)) {
                         User newUser = new User(fname, lname, email, pass); //call the user creation method
+                      String welcome = String.format("Welcome to Pupr, %s", newUser.getFirstName());
+                       registered.setText(welcome);
                     }
+
                 } catch(IOException | SQLException e) { //Catch undeclared IO and SQL exceptions by throwing a new Runtime Exception. Must do this because you can't declare these with the overridden onCreate or onClick methods
                     throw new RuntimeException(e);
 
