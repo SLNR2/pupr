2ea5145455feb49d0f4633af27dc43bc827cce69
Sloan Lipman <sloan.lipman@gmail.com>
2018-11-07 19:08:10 -0500

    Moved Dog into User and worked on uploading/saving pictures

--- Pupr_Android/app/src/main/java/com/pupr/CreateProfile.java
+++ Pupr_Android/app/src/main/java/com/pupr/CreateProfile.java
@@ -1,18 +1,31 @@
 package com.pupr;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CreateProfile extends AppCompatActivity {
     private static final int RESULT_LOAD_IMAGE = 1; //Allows for image to be returned
+    private static final String appName = "pupr"; //name of app, used for a directory name
+
+    private static Long tsLong = System.currentTimeMillis() / 1000; //used to generate a unique file name
+    private static String ts = tsLong.toString(); //put the time toString
+
 
     ImageView imageToUpload;
     EditText nameToUpload;
@@ -27,17 +40,34 @@ public class CreateProfile extends AppCompatActivity {
         nameToUpload = findViewById(R.id.new_dog_name);
         bioToUpload = findViewById(R.id.new_dog_bio);
         submitProfile = findViewById(R.id.submitDog);
-
+    //ClickListener to let you upload a picture
         imageToUpload.setOnClickListener(new View.OnClickListener() {
                                              @Override
                                              public void onClick(View v) {
-                                                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //Let user select an image from gallery
-                                                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                                                   uploadImage();
+                                                   new ImageSaver(v.getContext());
+
                                              }
                                          });
-               // submitProfile.setOnClickListener();
+        submitProfile.setOnClickListener(new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View v) {
+                                    //Save image
+                                    //Set image as an attribute for the user
+                                    //Load the Main Page
+
+
+                                }
+        });
     }
 
+    //Two methods that are used for uploading images
+    protected void uploadImage() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI); //Let user select an image from gallery
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+
+    }
 
     @Override
         protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -47,6 +77,4 @@ public class CreateProfile extends AppCompatActivity {
                 imageToUpload.setImageURI(selectedImage); //set image and display it
             }
         }
-
-
 }
--- Pupr_Android/app/src/main/java/com/pupr/User.java
+++ Pupr_Android/app/src/main/java/com/pupr/User.java
@@ -17,10 +17,35 @@ public class User {
     private String lastName = "";
     private String username = "";
     private String password = "";
-    public Queue<Dog> votingQueue = new LinkedList<>(); //Individual voting queue for each user
+
+
+    private String dogName = "";
+    private String bio = "";
+    private int totalScore = 0;
+    private int numberOfRatings = 0;
+    private double averageRating = 0;
+    private int ranking; //Might not need ranking? Can just return its position on leaderboard?
+    private Picture pic;
+
+    public String getDogName() {return this.dogName;}
+    public String getBio() {return this.bio;}
+    public Picture getPicture()  {return this.pic;}
+    public int getScore()  {return this.totalScore;}
+    public int getRatings() {return this.numberOfRatings;}
+    public double getAverage() {return this.averageRating;}
+
+
+    //Setter methods
+    public void setDogName(String newDogName) {this.dogName = newDogName;}
+    public void setBio(String newBio) {this.bio = newBio;}
+    public void setPic(Picture newPic) {this.pic = newPic;}
+
+
+
+
+    public Queue<User> votingQueue = new LinkedList<>(); //Individual voting queue for each user
     private static int numberOfUsers = 0; //Update the total number of users each time one is created or deleted... I forget why.
     private static int nextUser = 0;
-    public Dog dog = new Dog(); //Instantiate a dog that belongs to each user
 
     protected static ArrayList<User> userList = new ArrayList<User>(); //provide a list of users in a LinkedList structure for user authentication.
     //To log in, the system will have to trace the list to see if there is a match
@@ -47,7 +72,6 @@ public class User {
         //NEED TO GO BACK AND TEST THAT USING THE this OPERATOR ADDS THE OBJECT CORRECTLY!!!
 
         this.votedOn.add(this.userId); //adds user's own id to the votedOn list so that a user cannot vote on his or her own dog
-        this.dog.setId(userId);
     }
 
     public static void setUser(User user) {currentUser = user;} //update the current user of the session
